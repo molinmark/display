46280bb522f1e36a780cb662b7a9701782c4c3e7
Danko <116387462+DankoMoscow@users.noreply.github.com>
2022-12-07 20:34:45 +0300

    Add files via upload

--- testing.py
+++ testing.py
@@ -1,152 +1,205 @@
 import pygame
-import random
-import os
-import math
+import sys
+import game_rogue
 import pygame_menu
-import game_rogue_final
-BLACK = (0, 0, 0)
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+pygame.mixer.init()
+width = 1000
+height = 800
+screen = pygame.display.set_mode((width, height))
+def set_rases():
+    # тут в зависимости от выбора значения меняется раса, которую мы передаём в game_rogue.privetstvie()
 
-def display(room):
+    5
 
-    done = False
+class Display():
+    def __init__(self):
+        BLUE = (0, 0, 255)
+        game_rogue.privetstvie()
+        self.person1 = game_rogue.making_character()
+        self.N = 8  # количество комнат
+        self.rooms = [0] * self.N
+        self.monster = [0] * self.N
+        self.item = [0] * self.N
 
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
+        self.f1 = pygame.font.Font(None, 36)
+        self.text1 = self.f1.render('', True,
+                                    (180, 0, 0))
+        screen.blit(self.text1, (600, 50))
 
+
+    def paint_score(self):
+        self.pole = pygame.font.Font(None, 24)
+        self.pole_text  = self.pole.render('Здоровье '+ str(self.person1.health) +
+                                           ' скорость ' + str(self.person1.speed) , True, (180, 0, 0))
+
+        self.pole2 = pygame.font.Font(None, 24)
+        self.pole_text2 = self.pole2.render('Урон ' + str(self.person1.attack_damage) +
+                                          ' радиус атаки ' + str(self.person1.attack_range) , True, (180, 0, 0))
+        screen.blit(self.pole_text, (0,0))
+        screen.blit(self.pole_text2, (0,30))
+
+
+    def paint(self,room):
+        screen.fill(pygame.Color(0, 0, 0))
         for i in range(0, len(room.matrix)):
             for j in range(0, len(room.matrix[i])):
                 if room.matrix[i][j] == '╬':
                     pygame.draw.rect(screen, (0, 125, 255), pygame.Rect(j * 50, i * 50, 50, 50))
                 elif room.matrix[i][j] == '๏':
                     pygame.draw.rect(screen, (0, 225, 55), pygame.Rect(j * 50, i * 50, 50, 50))
-
                 elif room.matrix[i][j] == '☿':
-                    pygame.draw.rect(screen, (0, 225, 15), pygame.Rect(j * 50, i * 50, 50, 50))
+                    pygame.draw.rect(screen, (210, 125, 115), pygame.Rect(j * 50, i * 50, 50, 50))
+                elif room.matrix[i][j] == '†':
+                    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(j * 50, i * 50, 50, 50))
 
         pygame.display.flip()
 
 
-def update_position(room):
-    for i in range(0, len(room.matrix)):
-        for j in range(0, len(room.matrix[i])):
-            if room.matrix[i][j] == '๏':
-                print(room.X0)
-                room.X0 += 1
-                room.matrix[room.Y0][room.X0] = '๏'
+    def alg(self,D1):
+        while self.person1.health>0:
+            for i in range(self.N):
+                self.rooms[i] = game_rogue.room()
+                self.rooms[i].generation()
+                D1.paint(self.rooms[i])
+                list_items = ['Кинжал', 'Короткий лук', 'Великий меч', 'Длинный лук', 'Зелье лечения']
+                name_items = game_rogue.random.choice(list_items)
+                self.item[i] = game_rogue.Items(name_items)
+                self.item[i].parametres()
 
-def press(room):
-    pressed_keys = pygame.key.get_pressed()
+                n = i
+                self.monster[i] = game_rogue.making_monster()
 
-    if pressed_keys[pygame.K_RIGHT]:
-        room.X0  += 1
-        print(room.X0)
-    if pressed_keys[pygame.K_LEFT]:
-        room.X0 -= 1
+                while self.person1.health>0:
+                    self.paint_score()
+                    attack_action = False
+                    if (abs(self.rooms[n].X0_monster0 - self.rooms[n].X0) <= self.monster[n].attack_range) and (
+                            abs(self.rooms[n].Y0_monster0 - self.rooms[n].Y0) <= self.monster[n].attack_range) and self.monster[
+                        n].health > 0:
+                        self.monster[n].attack_result(self.person1)
+                        attack_action = True
+
+                        if self.person1.health <= 0:
+                            self.pole3 = pygame.font.Font(None, 30)
+                            self.pole_text3 = self.pole3.render('Вы погибли', True, (200, 100, 0))
+
+                            screen.blit(self.pole_text3, (width/2, height/2))
+
+                    if (abs(self.rooms[n].X0_monster0 - self.rooms[n].X0) <= self.person1.attack_range) and (
+                            abs(self.rooms[n].Y0_monster0 - self.rooms[n].Y0) <= self.person1.attack_range) and self.monster[n].health > 0:
+                        #текст
+                        self.f1 = pygame.font.Font(None, 36)
+                        self.text1 = self.f1.render('БЕЙ!(f)', True,
+                                                    (180, 0, 0))
+                        screen.blit(self.text1, (600, 50))
+                        pygame.display.flip()
+                        action = D1.button()
+                        if action == ('f' or 'F'):
+                            self.person1.attack_result(self.monster[n])
+
+                        elif action == ('g' or 'G') and attack_action == True:
+                            self.person1.start_defence(monster[n])
+
+                        elif action == ('g' or 'G') and attack_action == False:
+                            print('Враг не наносил Вам урон')
+                            if self.person1.health <= 0:
+                                print('Вы погибли, игра закончена')
+
+
+                    for j in range(self.person1.speed):
+                        self.paint_score()
+                        # текст
+                        self.f1 = pygame.font.Font(None, 36)
+                        self.text1 = self.f1.render('ИДИ!(wasd)', True,
+                                                    (180, 0, 0))
+                        screen.blit(self.text1, (600, 100))
+                        pygame.display.flip()
+
+                        new_cord = self.person1.move(D1.button(), self.rooms[n].cord0, self.rooms[n].cord,self.rooms[n].exit, self.rooms[n].input, self.rooms[n].cord_monster0)
+
+
+                        self.rooms[n].update_character(new_cord)
+                        if self.rooms[n].input[0] == self.rooms[n].cord0[0] and self.rooms[n].input[1] == self.rooms[n].cord0[1] and n != 0:
+                            n = n - 1
+
+                        elif self.rooms[n].exit[0] - 1 == self.rooms[n].cord0[0] and self.rooms[n].exit[1] == self.rooms[n].cord0[1]:
+                            n = n + 1
+
+                        if self.rooms[i].exit[0] - 1 == self.rooms[i].cord0[0] and self.rooms[i].exit[1] == self.rooms[i].cord0[1]:
+                            break
+                        D1.paint(self.rooms[n])
+
+                    if self.rooms[i].exit[0] - 1 == self.rooms[i].cord0[0] and self.rooms[i].exit[1] == self.rooms[i].cord0[1]:
+                        break
+
+                    if self.monster[n].health > 0:
+                        for m in range(self.monster[i].speed):
+                            new_cord_monster = self.monster[n].move(self.rooms[n].cord_monster0, self.rooms[n].cord, new_cord)
+                            self.rooms[n].update_monster(new_cord_monster, self.monster[i].alive_or_ded())
+                            D1.paint(self.rooms[n])
+                    else:
+                        self.rooms[n].update_monster(new_cord_monster, self.monster[n].alive_or_ded())
+                        D1.paint(self.rooms[n])
+
+
+                    if self.item[n].state == 'лежит':
+
+                        if (abs(new_cord_monster[0] - new_cord[0]) <= 1 and abs(
+                                new_cord_monster[1] - new_cord[1]) <= 1) and self.monster[n].alive_or_ded() == 'мёртв':
+                            # текст
+                            self.f1 = pygame.font.Font(None, 36)
+                            self.text1 = self.f1.render(f'БЕРИ {self.item[n].name_items}(e)!', True,
+                                                        (180, 0, 0))
+                            screen.blit(self.text1, (300, 100))
+                            pygame.display.flip()
+
+                            action_predmet = D1.button()
+                            if action_predmet == 'e':
+                                self.item[n].state = 'взят'
+                                characteristics = [self.item[n].range_attack, self.item[n].damage, self.item[n].health]
+                                self.person1.attack_range += characteristics[0]
+                                self.person1.attack_damage += characteristics[1]
+                                self.person1.health += characteristics[2]
+
+            print(D1.button())
+
+
+    def button(self):
+        while True:
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        return 1
+                    elif event.key == pygame.K_w:
+                        return 'w'
+                    elif event.key == pygame.K_a:
+                        return 'a'
+                    elif event.key == pygame.K_s:
+                        return 's'
+                    elif event.key == pygame.K_d:
+                        return 'd'
+                    elif event.key == pygame.K_f:
+                        return 'f'
+                    elif event.key == pygame.K_g:
+                        return 'g'
+                    elif event.key == pygame.K_e:
+                        return 'e'
 
 def main():
-    global N, rooms, monster, item, person1
-    game_rogue_final.privetstvie()
-    person1 = game_rogue_final.making_character()
-    print(f'''Имя: {person1.name}, уровень жизни : {person1.health}, уровень защиты: {person1.armor}, 
-дальность атаки: {person1.attack_range}, урон: {person1.attack_damage} и скорость: {person1.speed}''')
+    print(menu.widgets)
+    D1=Display()
+    D1.alg(D1)
 
-    N = 3  # количество комнат
-    rooms = [0] * N
-    monster = [0] * N
-    item = [0] * N
-    rooms[1] = game_rogue_final.room()
-    rooms[1].generation()
-    display(rooms[1])
-    while True:
-        print(rooms[1].X0)
-        #press(rooms[1])
 
-        #update_position(rooms[1])
-        pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[pygame.K_RIGHT]:
+menu = pygame_menu.Menu('Welcome', 600, 400, theme=pygame_menu.themes.THEME_BLUE)
+menu.add.text_input( 'Name :', name =  ' ')
+menu.add.selector('Раса :', [('Эльф', 1), ('Человек', 2), ('Гном', 3)], onchange = set_rases)
+menu.add.text_input( 'Добро пожаловать в подземелье')
+menu.add.button('Play', main)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+menu.mainloop(screen)
 
-            rooms[1].X0  += 1
-        elif pressed_keys[pygame.K_LEFT]:
-            rooms[1].X0 -= 1
-        display(rooms[1])
-        screen.fill(BLACK)
-        rooms[1].generation()
-        pygame.display.update()
-"""
-for i in range(N):
-    rooms[i] = game_rogue.room()
-    rooms[i].generation()
-    display()
-    list_items = ['Кинжал', 'Короткий лук', 'Великий меч','Длинный лук']
-    name_items = game_rogue.random.choice(list_items)
-    item[i] = game_rogue.Items(name_items)
-    item[i].parametres()
-    n = i
-    monster[i] = game_rogue.making_monster()
-    while True:
-        print(f'В начале хода у Вас: {person1.health} здоровья')
-        attack_action = False
-        if (abs(rooms[n].X0_monster0 - rooms[n].X0) <= monster[n].attack_range) and (abs(rooms[n].Y0_monster0 - rooms[n].Y0) <= monster[n].attack_range) and monster[n].health > 0:
-            monster[n].attack_result(person1)
-            attack_action = True
-            print(f'Здоровье персонажа после атаки: {person1.health}')
-            if person1.health <= 0:
-                print('Вы погибли, игра закончена')
-                raise SystemExit
-        if (abs(rooms[n].X0_monster0 - rooms[n].X0) <= person1.attack_range) and (
-                abs(rooms[n].Y0_monster0 - rooms[n].Y0) <= person1.attack_range) and monster[n].health > 0:
-            action = input('Желаете напасть на врага? Если да - нажмите f, если хотите защититься - нажмите g')
-            if action == ('f' or 'F'):
-                person1.attack_result(monster[n])
-                print(f'Здоровье монстра после атаки: {monster[n].health}')
-            elif action == ('g' or 'G') and attack_action == True:
-                person1.start_defence(monster[n])
-            elif action == ('g' or 'G') and attack_action == False:
-                print('Враг не наносил Вам урон')
-                if person1.health <= 0:
-                    print('Вы погибли, игра закончена')
-        if monster[n].health > 0:
-            print(f'Вы видите в комнате {monster[n].name}')
-        for j in range(person1.speed):
-            new_cord = person1.move(input('Введите напраление "w", "a", "s", "d"'), rooms[n].cord0, rooms[n].cord,
-                                    rooms[n].exit, rooms[n].input, rooms[n].cord_monster0)
-            rooms[n].update_character(new_cord)
-            if rooms[n].input[0] == rooms[n].cord0[0] and rooms[n].input[1] == rooms[n].cord0[1] and n != 0:
-                n = n - 1
-            elif rooms[n].exit[0] - 1 == rooms[n].cord0[0] and rooms[n].exit[1] == rooms[n].cord0[1]:
-                n = n + 1
-            if rooms[i].exit[0] - 1 == rooms[i].cord0[0] and rooms[i].exit[1] == rooms[i].cord0[1]:
-                break
-            rooms[n].display()
-        if rooms[i].exit[0] - 1 == rooms[i].cord0[0] and rooms[i].exit[1] == rooms[i].cord0[1]:
-            break
-            if monster[n].health > 0:
-                for m in range(monster[i].speed):
-                    new_cord_monster = monster[n].move(rooms[n].cord_monster0, rooms[n].cord, new_cord)
-                    rooms[n].update_monster(new_cord_monster, monster[i].alive_or_ded())
-                    rooms[n].display()
-            else:
-                rooms[n].update_monster(new_cord_monster, monster[n].alive_or_ded())
-                rooms[n].display()
-        print(monster[n].name, monster[n].alive_or_ded())
-        if item[n].state =='лежит':
-            item[n].loot(new_cord_monster, new_cord, monster[n].alive_or_ded())
-            if item[n].state == 'взят':
-                characteristics = [item[n].range_attack, item[n].damage]
-                person1.attack_range = characteristics[0]
-                person1.attack_damage = characteristics[1]
-print('ВЫ ПОБЕДИЛИ !!!')
-"""
-def make_menu():
-    menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
-    menu.add.text_input('Name :', default='John Doe')
-    #menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-    menu.add.button('Play', main())
-    menu.add.button('Quit', pygame_menu.events.EXIT)
-    menu.mainloop(screen)
-
-make_menu()
